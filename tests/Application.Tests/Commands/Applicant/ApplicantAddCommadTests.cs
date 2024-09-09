09cbe9ebaf2b8ed752c6e9714eb3978c94f3f5d9
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2024-09-09 11:29:34 +0100

    Adding unit tests

---
+++ tests/Application.Tests/Commands/Applicant/ApplicantAddCommadTests.cs
@@ -0,0 +1,25 @@
+﻿using Application.Commands.Applicant;
+using FluentAssertions;
+using MediatR;
+using NetArchTest.Rules;
+
+namespace Application.Tests.Commands.Applicant;
+public class ApplicantAddCommadHandlerTests
+{
+    [Fact]
+    public void ApplicantAddRequest_should_inherit_from_IRequest()
+    {
+        var tt = Types.InNamespace(typeof(ApplicantAddRequest).Namespace).That().HaveName(nameof(ApplicantAddRequest));
+        var result = tt
+            .Should()
+            .Inherit(typeof(IRequest)).GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void ApplicantAddResponse_should_inherit_from_IResponse()
+    {
+
+    }
+}
